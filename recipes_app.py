e740bbec42a436c303e53ca52b23fd1917ec1a3f
HoveyWanKenobi-95 <59634603+HoveyWanKenobi-95@users.noreply.github.com>
2023-06-14 12:01:56 -0400

    Update recipes_app.py
    match dtypes for sliders

--- recipes_app.py
+++ recipes_app.py
@@ -7,9 +7,9 @@ data = pd.read_parquet('AllRecipes_DB.parquet')
 
 list_of_clean_ingredients = pd.read_csv('distinct_ingredients.csv')['ingredients']
 
-ratings_cutoff = st.slider('Minimum Rating',min_value=0.0,max_value=5.0, value=4.0, step=0.5)
-prep_time_cutoff = st.slider('Maximum Prep Time', min_value = data.prep_time.min(), max_value =  data.prep_time.max(), value = 30., step=1.)
-total_time_cutoff = st.slider('Maximum Total Time', min_value = data.total_time.min(), max_value =  data.total_time.max(), value = 30., step=1.)
+ratings_cutoff = st.slider('Minimum Rating',min_value=float(0.0),max_value=float(5.0), value=float(4.0), step=float(0.5))
+prep_time_cutoff = st.slider('Maximum Prep Time', min_value = float(data.prep_time.min()), max_value =  float(data.prep_time.max()), value = float(30.), step=float(1.))
+total_time_cutoff = st.slider('Maximum Total Time', min_value = float(data.total_time.min()), max_value =  float(data.total_time.max()), value = float(30.), step=float(1.))
 ingredients_list = st.multiselect('Ingredients you have', options = list_of_clean_ingredients)
 
 if st.button('Get the Recipes'):
